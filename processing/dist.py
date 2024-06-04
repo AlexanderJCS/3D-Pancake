1d5624c5c70fe18328e87218578cc578f5cfef49
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-04 14:50:47 -0400

    add blurring

--- processing/dist.py
+++ processing/dist.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy import ndimage
 
-import visual
 
 def gen_dist_map(data: np.ndarray, xy_scale: float, z_scale: float) -> np.ndarray:
     # add padding around data to prevent edges not counting as 0
@@ -20,3 +19,18 @@ def gen_dist_map(data: np.ndarray, xy_scale: float, z_scale: float) -> np.ndarra
     dist_map_negatives = dist_map_negatives[1:-1, 1:-1, 1:-1]
 
     return dist_map_positives - dist_map_negatives
+
+
+def blur(dist_map: np.ndarray, c_s: float, scale_xy: float, scale_z: float) -> np.ndarray:
+    # Sigma formula from paper
+    sigma = c_s * np.max(dist_map.flatten())
+
+    # if there's any weird problems, try changing the sigma ratio to scale_z / scale_xy or change sigma to
+    # sigma_xy, sigma_xy, sigma_z
+
+    # Adjust standard deviations for each axis
+    sigma_xy = sigma * (scale_xy / scale_xy)
+    sigma_z = sigma * (scale_xy / scale_z)
+
+    # Apply anisotropic Gaussian blur
+    return ndimage.gaussian_filter(dist_map, sigma=(sigma_z, sigma_xy, sigma_xy))
