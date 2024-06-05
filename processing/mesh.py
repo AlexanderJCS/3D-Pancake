4c6aa3c1cad871bdad054f66d804ba4b1e410cc5
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-05 16:12:42 -0400

    OMG I ROTATED IT

--- processing/mesh.py
+++ processing/mesh.py
@@ -3,6 +3,8 @@ from . import obb
 import open3d as o3d
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 
 class Mesh:
     def __init__(self, bounding_box: obb.Obb, geom_center: np.ndarray):
@@ -11,4 +13,23 @@ class Mesh:
 
     @staticmethod
     def _gen(bounding_box: obb.Obb):
-        pass
+        rotation_matrix = bounding_box.rotation.as_matrix()
+        center = bounding_box.o3d_obb.center
+        vertices = bounding_box.vertices
+
+        print(center)
+
+        vertices -= center
+        vertices = bounding_box.vertices @ rotation_matrix
+        vertices += center
+
+        print(center)
+
+        print(bounding_box.o3d_obb.extent)
+
+        # plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.scatter(vertices[:, 0], vertices[:, 1], vertices[:, 2])
+        plt.show()
