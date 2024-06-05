8b6d7a32e4e3bcb43121271f7d4b1f5cec22c22b
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-05 15:55:43 -0400

    fix a thingymabob

--- processing/mesh.py
+++ processing/mesh.py
@@ -7,7 +7,7 @@ import numpy as np
 class Mesh:
     def __init__(self, bounding_box: obb.Obb, geom_center: np.ndarray):
         self.bounding_box = bounding_box
-        self.point_cloud = self._gen(bounding_box)
+        self.mesh = self._gen(bounding_box)
 
     @staticmethod
     def _gen(bounding_box: obb.Obb):
