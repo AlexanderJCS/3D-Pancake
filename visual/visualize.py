d67eed9c04e345977b1ceb646792c220e615162c
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-05 11:45:44 -0400

    update a lot of stuff tbh

--- visual/visualize.py
+++ visual/visualize.py
@@ -3,6 +3,9 @@ import numpy as np
 import open3d as o3d
 
 from processing import data
+from processing import obb
+
+from typing import Optional
 
 
 class SliceViewer:
@@ -57,13 +60,24 @@ class SliceViewer:
         plt.show(block=True)  # if plt.show is not blocking, it will cause the window to not respond
 
 
-def o3d_point_cloud(dist_map: np.ndarray, center: np.ndarray, scale: data.Scale):
-    point_cloud = o3d.geometry.PointCloud(
-        o3d.utility.Vector3dVector(np.argwhere(dist_map > 0) * scale.zyx())
-    )
+def o3d_point_cloud(
+        dist_map: np.ndarray,
+        scale: data.Scale,
+        obbs: Optional[list[obb.Obb]] = None,
+        center: Optional[np.ndarray] = None
+):
+    geometries = [
+        o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.argwhere(dist_map > 0) * scale.zyx()))
+    ]
 
-    # create a sphere to visualize the center
-    sphere = o3d.geometry.TriangleMesh().create_sphere(radius=max(dist_map.shape * scale.zyx()) / 75, resolution=20)
-    sphere.translate(center)
+    if obbs is not None:
+        obbs = [bounding_box.o3d_obb for bounding_box in obbs]
+        geometries.extend(obbs)
 
-    o3d.visualization.draw_geometries([sphere, point_cloud])
+    if center is not None:
+        # create a sphere to visualize the center
+        sphere = o3d.geometry.TriangleMesh().create_sphere(radius=max(dist_map.shape * scale.zyx()) / 75, resolution=20)
+        sphere.translate(center)
+        geometries.append(sphere)
+
+    o3d.visualization.draw_geometries(geometries)
