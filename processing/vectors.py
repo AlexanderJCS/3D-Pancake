def28332a7e1e7ceda6387c780c20021923bc98a
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-06 13:41:57 -0400

    fix order

--- processing/vectors.py
+++ processing/vectors.py
@@ -13,7 +13,7 @@ def gen_gradient(dist_map: np.ndarray, scale: data.Scale) -> np.ndarray:
     """
 
     # todo: verify scale.zyx() is the correct order and that all vectors are pointing the right way
-    gradient_x, gradient_y, gradient_z = np.gradient(dist_map, *scale.zyx())
+    gradient_z, gradient_y, gradient_x = np.gradient(dist_map, *scale.zyx())
 
     # todo: verify that xyz is the correct order
     return np.stack((gradient_x, gradient_y, gradient_z), axis=-1)
