fix off-by-one error where the edge datapoints (including z) is cut off by 1

mainformpancake3d.py
@@ -76,7 +76,11 @@ class MainFormPancake3D(OrsAbstractWindow):
         max_indices = np.array([max_indices.getX(), max_indices.getY(), max_indices.getZ()], dtype=int)[::-1]
 
         roi_arr = roi.getNDArray()
-        roi_arr = roi_arr[min_indices[0]:max_indices[0], min_indices[1]:max_indices[1], min_indices[2]:max_indices[2]]
+        roi_arr = roi_arr[
+                  min_indices[0]:max_indices[0] + 1,
+                  min_indices[1]:max_indices[1] + 1,
+                  min_indices[2]:max_indices[2] + 1
+        ]
 
         # Data processing
         processing.get_area(