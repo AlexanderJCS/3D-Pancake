a6a8a72147c4109925a5620ff42af08d3f005419
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-18 15:38:48 -0400

    UI overhaul + partial implementation of processing multiple rois

--- mainformpancake3d.py
+++ mainformpancake3d.py
@@ -1,4 +1,5 @@
-from ORSModel import orsObj, ROI
+import ORSModel
+from ORSModel import orsObj, ROI, MultiROI
 from ORSServiceClass.ORSWidget.chooseObjectAndNewName.chooseObjectAndNewName import ChooseObjectAndNewName
 from PyQt6.QtCore import pyqtSlot, Qt
 from PyQt6 import QtGui
@@ -10,6 +11,7 @@ from PyQt6.QtWidgets import QDialog
 from .ui_mainformpancake3d import Ui_MainFormPancake3D
 
 from typing import Optional
+from typing import Union
 
 from processing import processing
 from processing import data
@@ -35,19 +37,13 @@ class MainFormPancake3D(OrsAbstractWindow):
             self
         )
 
+        self.selected_roi: Union[ROI, MultiROI, None] = None
+
     @staticmethod
-    def roi_dialog() -> Optional[ROI]:
-        def acceptable_roi(roi_to_test: ROI):
-            # The ROI should not be empty
-            if roi_to_test.getVoxelCount(0) == 0:
-                return False
-
-            return True
-
+    def roi_dialog(managed_class: Union[type[ROI], type[MultiROI]] = ROI) -> Optional:
         chooser = ChooseObjectAndNewName(
-            managedClass=[ROI],
-            parent=WorkingContext.getCurrentContextWindow(),
-            filterFunction=acceptable_roi
+            managedClass=[managed_class],
+            parent=WorkingContext.getCurrentContextWindow()
         )
 
         chooser.setWindowTitle("Select an ROI")
@@ -63,13 +59,24 @@ class MainFormPancake3D(OrsAbstractWindow):
 
         return roi
 
-    @pyqtSlot()
-    def on_btn_process_clicked(self):
-        roi: ROI = self.roi_dialog()
+    def select_roi(self, managed_class: Union[type[ROI], type[MultiROI]]):
+        roi: Optional[managed_class] = self.roi_dialog(managed_class)
 
         if roi is None:
             return
 
+        self.selected_roi = roi
+        self.ui.label_selected.setText(f"Selected: {roi.getTitle()}")
+
+    @pyqtSlot()
+    def on_btn_select_roi_clicked(self):
+        self.select_roi(ROI)
+
+    @pyqtSlot()
+    def on_btn_select_multiroi_clicked(self):
+        self.select_roi(MultiROI)
+
+    def process_single_roi(self, roi: ROI):
         min_indices = roi.getLocalBoundingBoxMin(0)
         min_indices = np.array([min_indices.getX(), min_indices.getY(), min_indices.getZ()], dtype=int)[::-1]
         max_indices = roi.getLocalBoundingBoxMax(0)
@@ -89,3 +96,59 @@ class MainFormPancake3D(OrsAbstractWindow):
             visualize=self.ui.chk_visualize.isChecked(),
             c_s=float(self.ui.line_edit_c_s.text())
         )
+
+    def process_multi_roi(self, multi_roi: MultiROI, scale: data.Scale):
+        roi_arr = multi_roi.getAsNDArray()
+
+        for label in range(1, multi_roi.getLabelCount() + 1):
+            label_bounding_box: ORSModel.Box = multi_roi.getBoundingBoxOfLabel(0, label)
+
+            min_indices: ORSModel.ors.Vector3 = label_bounding_box.getSummitmmm()
+            max_indices: ORSModel.ors.Vector3 = label_bounding_box.getSummitppp()
+
+            min_indices = np.array([min_indices.getX(), min_indices.getY(), min_indices.getZ()])[::-1]
+            max_indices = np.array([max_indices.getX(), max_indices.getY(), max_indices.getZ()])[::-1]
+
+            min_indices *= 1e9  # meters to nm
+            max_indices *= 1e9
+
+            min_indices /= scale.zyx()
+            max_indices /= scale.zyx()
+
+            min_indices = min_indices.astype(int)
+            max_indices = max_indices.astype(int)
+
+            print(min_indices)
+            print(max_indices)
+
+            print(min_indices[0])
+            print(max_indices[0])
+            print(roi_arr[0][0][0])
+
+            new_arr = roi_arr[
+                    min_indices:max_indices[0] + 1,
+                    min_indices[1]:max_indices[1] + 1,
+                    min_indices[2]:max_indices[2] + 1
+            ]
+
+            print(new_arr.shape)
+            print(new_arr)
+            print(new_arr.max())
+            print(new_arr.min())
+            print(new_arr.mean())
+
+    @pyqtSlot()
+    def on_btn_process_clicked(self):
+        # TODO: Add error handling for invalid scale input
+
+        if self.selected_roi is None:
+            self.ui.label_output.setText("No ROI selected")
+            return
+
+        if isinstance(self.selected_roi, ROI):
+            self.process_single_roi(self.selected_roi)
+        else:
+            self.process_multi_roi(
+                self.selected_roi,
+                data.Scale(float(self.ui.line_edit_xy_scale.text()), float(self.ui.line_edit_z_scale.text()))
+            )
--- quick_run.py
+++ quick_run.py
@@ -10,7 +10,7 @@ from processing import data
 
 
 def run():
-    with open("data/roi.npy", "rb") as f:
+    with open("data/big_hole.npy", "rb") as f:
         roi = np.load(f)
 
     # note: c_s = 0.2 provides good results in my experience, but c_s = 0.67 is default
