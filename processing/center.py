d37837751c3afed33647f3b4dc2be804550532aa
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-26 10:39:25 -0400

    correct center code

--- processing/center.py
+++ processing/center.py
@@ -5,28 +5,23 @@ from .data import meta
 
 def geom_center(dist_map: np.array, scale: meta.Scale) -> np.ndarray:
     """
-    Finds the geometric center of the PSD using a weighted average
+    Finds the geometric center of the PSD using the geometric center of the top 20% of the PSD distance map
 
     :param dist_map: The distance map
     :param scale: The scale of a voxel
     :return: A 3-element float array containing the geometric center in XYZ coordinates
     """
 
-    # Normalize the grid values so they add up to 1
-    grid_normalized = dist_map - np.min(dist_map)
-    grid_normalized = grid_normalized / np.sum(grid_normalized)
+    # Find the top 20% of the PSD
+    top_20 = np.percentile(dist_map, 80)
 
-    # Create arrays of coordinates
-    z, y, x = np.indices(dist_map.shape).astype(float)
+    # Find the indices of the top 20% of the PSD
+    top_20_indices = np.where(dist_map >= top_20)
 
-    # Account for x, y, and z scales
-    x *= scale.xy
-    y *= scale.xy
-    z *= scale.z
+    # Find the geometric center of the top 20% of the PSD
+    center = np.mean(top_20_indices, axis=1)
 
-    # Calculate the weighted average of the x and y coordinates
-    center_x = np.sum(x * grid_normalized)
-    center_y = np.sum(y * grid_normalized)
-    center_z = np.sum(z * grid_normalized)
+    # Convert the geometric center from voxel space to world space
+    center = center * scale.zyx()
 
-    return np.array([center_x, center_y, center_z])
+    return center[::-1]
