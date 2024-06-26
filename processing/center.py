2f9df8ef1119351d8cc3baf74ce44de86829df28
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-26 12:57:42 -0400

    top 1% of vertices instead of top 20% to improve geometric center calculation

--- processing/center.py
+++ processing/center.py
@@ -5,18 +5,18 @@ from .data import meta
 
 def geom_center(dist_map: np.array, scale: meta.Scale) -> np.ndarray:
     """
-    Finds the geometric center of the PSD using the geometric center of the top 20% of the PSD distance map
+    Finds the geometric center of the PSD using the geometric center of the top 1% of the PSD distance map
 
     :param dist_map: The distance map
     :param scale: The scale of a voxel
     :return: A 3-element float array containing the geometric center in XYZ coordinates
     """
 
-    # Find the top 20% of the PSD
-    top_20 = np.percentile(dist_map, 80)
+    # Find the top 1% of the PSD
+    top_vertices = np.percentile(dist_map, 99)
 
-    # Find the indices of the top 20% of the PSD
-    top_20_indices = np.where(dist_map >= top_20)
+    # Find the indices of the top 1% of the PSD
+    top_20_indices = np.where(dist_map >= top_vertices)
 
     # Find the geometric center of the top 20% of the PSD
     center = np.mean(top_20_indices, axis=1)
