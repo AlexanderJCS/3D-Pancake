c3beae4cbc008ee1afd7317f7137e1a054888eac
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-06-26 10:17:52 -0400

    display P value

--- test/time_test.py
+++ test/time_test.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
 import accuracy
 
@@ -39,22 +40,18 @@ def plot_times_vs_voxels(name, times, num_voxels) -> None:
         ax.scatter(num_voxels[i], times[i], color=colors[i], label=txt)
 
     # Fit a line to the data
-    coefficients = np.polyfit(num_voxels, times, 1)
-    polynomial = np.poly1d(coefficients)
-    xs = np.linspace(min(num_voxels), max(num_voxels), 1000)
-    ys = polynomial(xs)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(num_voxels, times)
 
     # Plot the line of best fit
-    ax.plot(xs, ys, color='red')
+    x_values = [0, max(num_voxels)]
+    y_values = [intercept, intercept + slope * max(num_voxels)]
 
-    # Calculate the R-squared value
-    correlation_matrix = np.corrcoef(num_voxels, times)
-    correlation_xy = correlation_matrix[0, 1]
-    r_squared = correlation_xy**2
+    ax.plot(x_values, y_values, color="black")
 
     # Display the correlation statistics
-    ax.text(0.05, 0.925, f'R² = {r_squared:.2f}', transform=ax.transAxes, fontsize=12)
-    ax.text(0.05, 0.875, f'y = {coefficients[0]:.2f}x + {coefficients[1]:.2f}', transform=ax.transAxes, fontsize=12)
+    ax.text(0.05, 0.925, f'R² = {r_value**2:.2f}', transform=ax.transAxes, fontsize=12)
+    ax.text(0.05, 0.875, f'P = {p_value:.4f}', transform=ax.transAxes, fontsize=12)
+    ax.text(0.05, 0.825, f'y = {slope:.2f}x + {intercept:.2f}', transform=ax.transAxes, fontsize=12)
 
     ax.set_xlabel("Number of Voxels")
     ax.set_ylabel("Time (s)")
