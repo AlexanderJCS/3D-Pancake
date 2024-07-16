5bc28f271247add5aafec51782521dbb392fa809
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-07-16 15:57:30 -0400

    o3d concave hull only computes once

--- playground/concave_hull_o3d.py
+++ playground/concave_hull_o3d.py
@@ -22,14 +22,6 @@ pcd.points = o3d.utility.Vector3dVector(np.argwhere(roi_exterior)[:, ::-1] * sca
 
 o3d.visualization.draw_geometries([pcd])
 
-mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, 0.002)
+mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha=1)
 mesh.compute_vertex_normals()
-o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
-
-tetra_mesh, pt_map = o3d.geometry.TetraMesh.create_from_point_cloud(pcd)
-for alpha in np.logspace(np.log10(0.5), np.log10(0.01), num=15):
-    print(f"alpha={alpha:.3f}")
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(
-        pcd, alpha, tetra_mesh, pt_map)
-    mesh.compute_vertex_normals()
-    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True, mesh_show_wireframe=True)
