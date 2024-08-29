556844ab5d7c447968fd38a085b78200ad10ab31
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-08-29 15:03:25 -0400

    bugfix: cannot open plugin when in non-development mode

--- ui_mainformpancake3d.py
+++ ui_mainformpancake3d.py
@@ -88,7 +88,7 @@ class Ui_MainFormPancake3D(object):
         self.btn_select_roi.setText(_translate("MainFormPancake3D", "Select Single PSD (ROI)"))
         self.label_selected.setText(_translate("MainFormPancake3D", "Selected: None"))
         self.btn_process.setText(_translate("MainFormPancake3D", "Process"))
-from copyable_label import CopyableLabel
+from .copyable_label import CopyableLabel
 
 
 if __name__ == "__main__":
