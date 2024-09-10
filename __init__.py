c0fa4cba44bd622abda490910e4473ab385747e9
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-09-10 14:49:11 -0400

    debugging code

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
+# redirect stdout and stderr to files in this dir
+import sys
+sys.stdout = open('C:\\stdout.txt', 'w')
+sys.stderr = open('C:\\stderr.txt', 'w')
+
 from .Pancake3D_eae430b521c411efa291f83441a96bd5 import Pancake3D_eae430b521c411efa291f83441a96bd5
