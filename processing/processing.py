efb8138e032a16cef9f51f7bacf395163febd34b
Alex <alexandercastronovo@gmail.com>
2024-10-21 17:57:12 -0400

    code cleanup

--- processing/processing.py
+++ processing/processing.py
@@ -47,6 +47,48 @@ class PancakeOutput:
         return self.area_nm / 1e6
 
 
+def visualize_step(
+        visualize: bool, visualize_signal, step_name: str, psd_data: np.ndarray, scale: data.Scale,
+        obb=None, center_point=None, psd_mesh=None, vectors_arr=None, vector=None):
+    """
+    Helper function to handle visualization. Only visualizes if visualize=True
+    
+    :param visualize: Whether to visualize the step. The function does nothing if False
+    :param visualize_signal: The signal to emit the visualization to. Used for PyQt
+    :param step_name: The name of the step
+    :param psd_data: The data to visualize
+    :param scale: The voxel spacing
+    :param obb: The oriented bounding box to visualize
+    :param center_point: The center of the data
+    :param psd_mesh: The mesh to visualize
+    :param vectors_arr: The vectors to visualize
+    :param vector: The vector to visualize
+    
+    """
+    if not visualize:
+        return
+        
+    if visualize_signal:
+        visualize_signal.emit(functools.partial(
+            visual.vis_3d,
+            psd_data, scale, step_name,
+            obb=obb,
+            center=center_point,
+            psd_mesh=psd_mesh,
+            vectors=vectors_arr,
+            vector=vector
+        ))
+    else:
+        visual.vis_3d(
+            psd_data, scale, step_name,
+            obb=obb,
+            center=center_point,
+            psd_mesh=psd_mesh,
+            vectors=vectors_arr,
+            vector=vector
+        )
+
+
 def get_area(
         raw_data: np.ndarray, scale: data.Scale, visualize: bool = False, c_s: float = 0.67,
         visualize_end: bool = False, visualize_unclipped: bool = False,
@@ -66,9 +108,7 @@ def get_area(
     :param visualize_signal: The signal to emit the visualization to. Used for PyQt
     :return: A PancakeOutput class, containing surface area and a bunch of other data
     """
-
-    # TODO: code cleanup - this function is very long and can be shortened easily through extraction
-
+    
     # Step A: load and format data
     formatted = data.format_data(raw_data)
 
@@ -78,26 +118,14 @@ def get_area(
     # Step Ba: Expand the dataset so the OBB does not have values outside the dataset
     formatted, translations = obb.expand_data(scale, formatted)
 
-    if visualize:
-        if visualize_signal:
-            visualize_signal.emit(functools.partial(
-                visual.vis_3d,
-                formatted, scale, "Step B: Formatted Data",
-                obb=obb
-            ))
-        else:
-            visual.vis_3d(
-                formatted, scale, "Step B: OBB",
-                obb=obb
-            )
+    visualize_step(visualize, visualize_signal, "Step A: Formatted Data", formatted, scale, obb=obb)
 
     # Step C: distance map
     distance_map = dist.gen_dist_map(formatted, scale)
     blurred = dist.blur(distance_map, c_s, scale)
-
+    
+    # don't show if it needs to be emitted to a signal since matplotlib doesn't play well with PyQt
     if visualize and not visualize_signal:
-        # don't show in the signal since matplotlib doesn't play well with PyQt
-        
         visualizer = visual.SliceViewer(distance_map)
         visualizer.visualize()
 
@@ -107,110 +135,34 @@ def get_area(
     # Step E: create the mesh
     psd_mesh = mesh.Mesh(obb, center_point, scale)
 
-    if visualize:
-        if visualize_signal:
-            visualize_signal.emit(functools.partial(
-                visual.vis_3d,
-                distance_map, scale, "Step E: Mesh",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh
-            ))
-        else:
-            visual.vis_3d(
-                distance_map, scale, "Step E: Mesh",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh
-            )
+    visualize_step(visualize, visualize_signal, "Step E: Mesh", distance_map, scale, obb=obb,
+                   center_point=center_point, psd_mesh=psd_mesh)
 
     # Step F: calculate gradient
     gradient = vectors.gen_gradient(blurred, scale)
 
-    if visualize:
-        if visualize_signal:
-            visualize_signal.emit(functools.partial(
-                visual.vis_3d,
-                distance_map, scale, "Step F: Gradient",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh,
-                vectors=gradient
-            ))
-        else:
-            visual.vis_3d(
-                distance_map, scale, "Step F: Gradient",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh,
-                vectors=gradient
-            )
+    visualize_step(visualize, visualize_signal, "Step F: Gradient", distance_map, scale, obb=obb,
+                   center_point=center_point, psd_mesh=psd_mesh, vectors_arr=gradient)
 
     # Step G: project gradient onto normal
     normal = obb.get_normal()
     projected_gradient = vectors.project_on_normal(gradient, normal)
 
-    if visualize:
-        if visualize_signal:
-            visualize_signal.emit(functools.partial(
-                visual.vis_3d,
-                distance_map, scale, "Step G: Projected Gradient",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh,
-                vector=[obb.o3d_obb.center, normal],
-                vectors=projected_gradient
-            ))
-        else:
-            visual.vis_3d(
-                distance_map, scale, "Step G: Projected Gradient",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh,
-                vector=[obb.o3d_obb.center, normal],
-                vectors=projected_gradient
-            )
+    visualize_step(visualize, visualize_signal, "Step G: Projected Gradient", distance_map, scale, obb=obb,
+                   center_point=center_point, psd_mesh=psd_mesh, vectors_arr=projected_gradient)
 
     # Step H: deform the mesh
     while psd_mesh.error() > 0.1:
         psd_mesh.deform(projected_gradient, scale)
 
-    if visualize or visualize_unclipped:
-        if visualize_signal:
-            visualize_signal.emit(functools.partial(
-                visual.vis_3d,
-                distance_map, scale, "Step H: Deformed Mesh",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh,
-                vectors=projected_gradient
-            ))
-        else:
-            visual.vis_3d(
-                distance_map, scale, "Step H: Deformed Mesh",
-                center=center_point,
-                obb=obb,
-                psd_mesh=psd_mesh,
-                vectors=projected_gradient
-            )
+    visualize_step(visualize or visualize_unclipped, visualize_signal, "Step H: Deformed Mesh", distance_map,
+                   scale, obb=obb, center_point=center_point, psd_mesh=psd_mesh, vectors_arr=projected_gradient)
 
     # Step I: move the vertices into the nearest OBB
     psd_mesh.clip_vertices(formatted, scale, dist_threshold)
 
-    if visualize or visualize_end:
-        if visualize_signal:
-            visualize_signal.emit(functools.partial(
-                visual.vis_3d,
-                distance_map, scale, "Step I: Clipped Vertices",
-                obb=obb,
-                psd_mesh=psd_mesh
-            ))
-        else:
-            visual.vis_3d(
-                distance_map, scale, "Step I: Clipped Vertices",
-                obb=obb,
-                psd_mesh=psd_mesh
-            )
+    visualize_step(visualize or visualize_end, visualize_signal, "Step I: Clipped Vertices", distance_map,
+                   scale, obb=obb, center_point=center_point, psd_mesh=psd_mesh)
 
     return PancakeOutput(
         psd_mesh.area(),
