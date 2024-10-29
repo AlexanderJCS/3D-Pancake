a56d6dce2c8d6f00894f1ef6ba0c672045dbd664
Alex <alexandercastronovo@gmail.com>
2024-10-29 19:04:41 -0400

    remove unused code

--- processing/vectors.py
+++ processing/vectors.py
@@ -27,27 +27,3 @@ def project_on_normal(gradient: np.array, normal: np.array) -> np.array:
 
     magnitudes = np.dot(gradient, normal)
     return normal * magnitudes[:, :, :, np.newaxis]
-
-
-def project_points(points: np.array, vec_x: np.array) -> np.array:
-    """
-    Projects the points (in [[x, y, z], [...], ...] format onto a new coordinate system. Vec represents the
-    x direction of the new coordinate system.
-
-    :param points: The points to project onto the vector in [[x, y, z], [...], ...] format
-    :param vec_x: The x-direction vector of the new coordinate system in [x, y, z] format
-    :return: The projected points in the same format as points
-    """
-
-    vec_x = vec_x / np.linalg.norm(vec_x)
-    vec_y = np.cross(np.array([0, 0, 1]), vec_x) / np.linalg.norm(np.cross(np.array([0, 0, 1]), vec_x))
-    vec_z = np.cross(vec_x, vec_y) / np.linalg.norm(np.cross(vec_x, vec_y))
-
-    return np.stack(
-        [
-            np.dot(points, vec_x),
-            np.dot(points, vec_y),
-            np.dot(points, vec_z)
-        ],
-        axis=-1
-    )
