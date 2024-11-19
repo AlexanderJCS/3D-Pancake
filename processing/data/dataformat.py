783a2e3f23800ebe3714aa60c4af75dd9e424b88
Alexander Castronovo <alexandercastronovo@gmail.com>
2024-11-19 13:39:36 -0500

    fix crash with time test

--- processing/data/dataformat.py
+++ processing/data/dataformat.py
@@ -2,11 +2,6 @@ import numpy as np
 
 from . import meta
 
-if __package__.count(".") == 0:
-    from log import logger
-else:
-    from ...log import logger
-
 
 def format_data(data: np.ndarray, scale: meta.Scale) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -21,14 +16,11 @@ def format_data(data: np.ndarray, scale: meta.Scale) -> tuple[np.ndarray, np.nda
     data_coords = np.argwhere(data)
 
     if data_coords.size == 0:
-        logger.warning("Data is empty")
         return data, np.array([0, 0, 0])
 
     min_xyz = np.min(data_coords, axis=0)
     max_xyz = np.max(data_coords, axis=0)
 
     translations = min_xyz * scale.xyz()
-
-    logger.info(f"Cropping data to remove empty space. {translations=} {min_xyz=} {max_xyz=}")
 
     return data[min_xyz[0]:max_xyz[0] + 1, min_xyz[1]:max_xyz[1] + 1, min_xyz[2]:max_xyz[2] + 1], translations
